ad143070bd484e360c52604f21bd5767ed85cd34
Lala-pukuchan <ruruover1105@gmail.com>
2023-12-28 19:05:55 +0100

    ex07

--- ex07/benchmark_train.py
+++ ex07/benchmark_train.py
@@ -7,50 +7,6 @@ from ex06.ridge import MyRidge as MyRidge
 from ex07.data_spliter import data_spliter
 
 
-# # Store performance of models
-# models_performance = {}
-
-# # Train and evaluate models with polynomial features up to degree 4
-# for degree in range(1, 5):
-#     models_performance[degree] = {}
-
-#     # lambda_values = np.arange(0, 1.1, 0.2)
-
-#     # for l in lambda_values:
-
-#     # Store model and performance
-#     # models_performance[degree][l] = {
-#     models_performance[degree] = {
-#         "model": model,
-#         "train_mse": train_mse,
-#         "cv_mse": cv_mse,
-#         "x_test_poly": x_test_poly,
-#         "y_test": y_test,
-#     }
-
-
-# # Save the models to a file
-# with open("ex07/models.pkl", "wb") as file:
-#     pickle.dump(models_performance, file)
-
-
-# # Plotting MSE for each model
-# for degree in models_performance:
-#     lambdas = sorted(models_performance[degree].keys())
-#     # test_mses = [models_performance[degree][l]["cv_mse"] for l in lambdas]
-#     test_mses = [models_performance[degree]["cv_mse"]]
-
-#     # Plotting the test MSE for each lambda
-#     # plt.plot(lambdas, test_mses, label=f"Degree {degree}", marker="o")
-#     plt.plot(degree, test_mses, "bo-")
-
-# plt.xlabel("Lambda")
-# plt.ylabel("CV MSE")
-# plt.title("CV MSE vs Lambda for Different Polynomial Degrees")
-# plt.legend()
-# plt.savefig("ex07/figure-1.png")
-
-
 def benchmark_train():
     # load data
     data = pd.read_csv("ex07/space_avocado.csv")
@@ -59,10 +15,10 @@ def benchmark_train():
 
     # split data
     x_train_val, x_test, y_train_val, y_test = data_spliter(x, y, 0.8)
-    x_train, x_cv, y_train, y_cv = data_spliter(x_train_val, y_train_val, 0.6)
+    x_train, x_cv, y_train, y_cv = data_spliter(x_train_val, y_train_val, 0.75)
 
-    # data storage
-    mse_values = []
+    # Store performance of models
+    models_performance = {}
 
     # Normalization
     min = x_train.min(axis=0)
@@ -77,58 +33,74 @@ def benchmark_train():
 
     # loop with each degree
     degrees = [1, 2, 3, 4]
+    smallest_mse = float("inf")
     for degree in degrees:
+
+        # storage for each degree
+        models_performance[degree] = {}
+
         # add polynomial feature
         x_train_poly = add_polynomial_features(x_train_scaled, degree)
         x_test_poly = add_polynomial_features(x_test_scaled, degree)
         x_cv_poly = add_polynomial_features(x_cv_scaled, degree)
 
-        # initiate ridge regression class
-        thetas = np.random.rand(x_train_poly.shape[1] + 1, 1)
-        lr = MyRidge(thetas, alpha=0.1, max_iter=1000)
+        # loop with each lambda
+        lambda_values = np.arange(0, 1.1, 0.2)
+        for l in lambda_values:
+            # initiate ridge regression class
+            thetas = np.random.rand(x_train_poly.shape[1] + 1, 1)
+            model = MyRidge(thetas, alpha=0.1, max_iter=1000, lambda_=l)
 
-        # train model
-        lr.fit_(x_train_poly, y_train)
+            # train model
+            model.fit_(x_train_poly, y_train)
 
-        # cross validate with normalized mse
-        mse = lr.mse_(y_cv, lr.predict_(x_cv_poly))
-        mse_values.append(mse / variance)
+            # cross validate with normalized mse
+            mse = model.mse_(y_cv, model.predict_(x_cv_poly)) / variance
 
-    plot_evaluation_curve(degrees, mse_values)
+            # print
+            print(f"degree: {degree}, lambda: {l}, mse: {mse}")
+            if (mse < smallest_mse):
+                smallest_mse = mse
+                best_degree = degree
+                best_lambda = l
 
+            # Store model and performance
+            models_performance[degree][l] = {
+                "model": model,
+                "mse": mse,
+                "x_test_poly": x_test_poly,
+                "y_test": y_test,
+            }
 
-def plot_evaluation_curve(degrees, mse_values):
-    plt.plot(degrees, mse_values, "bo-")
-    plt.xlabel("Degree of Polynomial")
-    plt.ylabel("Mean Squared Error")
-    plt.title("Evaluation Curve")
+    # Print the best model
+    print(f"Best model: degree {best_degree}, lambda {best_lambda}, mse {smallest_mse}")
+    
+    # Save the models to a file
+    with open("ex07/models.pkl", "wb") as file:
+        pickle.dump(models_performance, file)
+
+def plot_evaluation_curve():
+    # Open the file and load the data
+    with open("ex07/models.pkl", "rb") as file:
+        models_performance = pickle.load(file)
+
+    # Plotting MSE for each model
+    for degree in models_performance:
+        lambdas = sorted(models_performance[degree].keys())
+        test_mses = [models_performance[degree][l]["mse"] for l in lambdas]
+
+        # Plotting the test MSE for each lambda
+        plt.plot(lambdas, test_mses, label=f"Degree {degree}", marker="o")
+
+    plt.xlabel("Lambda")
+    plt.ylabel("MSE")
+    plt.title("MSE vs Lambda for Different Polynomial Degrees")
+    plt.legend()
     plt.savefig("ex07/evaluation_curve.png")
 
 
-#def plot_predictions(model, x_test_poly, y_test):
-#    print(x_test_poly.shape, y_test.shape)
-#    y_pred = model.predict_(x_test_poly)
-
-#    fig = plt.figure()
-#    ax = fig.add_subplot(111, projection="3d")
-#    ax.scatter(x_test_poly[:, 1], x_test_poly[:, 2], y_test, c="b", label="True Price")
-#    ax.scatter(
-#        x_test_poly[:, 1], x_test_poly[:, 2], y_pred, c="r", label="Predicted Price"
-#    )
-#    ax.set_xlabel("Weight")
-#    ax.set_ylabel("Production Distance")
-#    ax.set_zlabel("Price")
-#    ax.legend()
-#    plt.title("True Price vs Predicted Price")
-#    plt.show()
-
-#    print(
-#        x_test_poly.shape,
-#    )
-#    print(
-#        x_test_poly,
-#    )
-
-
 if __name__ == "__main__":
+    # train model and save to file
     benchmark_train()
+    # plot evaluation curve for all models
+    plot_evaluation_curve()
--- ex07/space_avocado.py
+++ ex07/space_avocado.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import pickle
 import matplotlib.pyplot as plt
-from benchmark_train import benchmark_train
+
 
 # Load dataset
 data = pd.read_csv("ex07/space_avocado.csv")
@@ -13,71 +13,41 @@ y = np.array(data["target"]).reshape(-1, 1)
 with open("ex07/models.pkl", "rb") as file:
     models = pickle.load(file)
 
-# Take best model
+# Identify the best degree
 best_mse = float("inf")
-best_model = None
+best_degree = None
 for degree in models:
     for lambda_, model_info in models[degree].items():
-        if model_info["cross_valid_mse"] < best_mse:
-            best_mse = model_info["cross_valid_mse"]
-            best_model = model_info["model"]
+        if model_info["mse"] < best_mse:
+            best_mse = model_info["mse"]
+            best_degree = degree
 
-x_test_poly = best_model["x_test_poly"]
-y_test = best_model["y_test"]
+# Print the best degree and MSE
+print(f"Best degree: {best_degree}, Best MSE: {best_mse}")
 
-# Predict using the best model
-predictions = best_model.predict_(x_test_poly)
+# Get models of the best degree with different lambda values
+lambda_models = models[best_degree]
 
-# Plot actual and predict
-y_pred = best_model.predict_(x_test_poly)
+# Create subplots for each lambda
+fig, axs = plt.subplots(1, len(lambda_models), figsize=(15, 5), subplot_kw={'projection': '3d'})
 
-# Plot 1: Feature 1 (Weight) and Feature 2 (Production Distance)
-fig1 = plt.figure()
-ax1 = fig1.add_subplot(111, projection="3d")
-ax1.scatter(
-    x_test_poly[:, 0], x_test_poly[:, 1], y_test[:, 0], c="b", label="Actual Price"
-)
-ax1.scatter(
-    x_test_poly[:, 0], x_test_poly[:, 1], y_pred[:, 0], c="r", label="Predicted Price"
-)
-ax1.set_xlabel("Weight")
-ax1.set_ylabel("Production Distance")
-ax1.set_zlabel("Price")
-ax1.legend()
-plt.title("Price Prediction with Weight and Production Distance")
-plt.savefig("results/ex10/result_ex10_figure-1.png")
-plt.close()
+for i, (lambda_, model_info) in enumerate(lambda_models.items()):
+    model = model_info["model"]
+    x_test_poly = model_info["x_test_poly"]
+    y_test = model_info["y_test"]
+    
+    # Predict using the model
+    y_pred = model.predict_(x_test_poly)
 
-# Plot 2: Feature 1 (Weight) and Feature 3 (Time Delivery)
-fig2 = plt.figure()
-ax2 = fig2.add_subplot(111, projection="3d")
-ax2.scatter(
-    x_test_poly[:, 0], x_test_poly[:, 2], y_test[:, 0], c="b", label="Actual Price"
-)
-ax2.scatter(
-    x_test_poly[:, 0], x_test_poly[:, 2], y_pred[:, 0], c="r", label="Predicted Price"
-)
-ax2.set_xlabel("Weight")
-ax2.set_ylabel("Time Delivery")
-ax2.set_zlabel("Price")
-ax2.legend()
-plt.title("Price Prediction with Weight and Time Delivery")
-plt.savefig("results/ex10/result_ex10_figure-2.png")
-plt.close()
+    # Scatter plot for actual vs predicted prices
+    axs[i].scatter(x_test_poly[:, 0], x_test_poly[:, 1], y_test[:, 0], c="b", label="Actual Price")
+    axs[i].scatter(x_test_poly[:, 0], x_test_poly[:, 1], y_pred[:, 0], c="r", label="Predicted Price")
+    axs[i].set_title(f"Lambda {lambda_}")
+    axs[i].set_xlabel("Weight")
+    axs[i].set_ylabel("Production Distance")
+    axs[i].set_zlabel("Price")
+    axs[i].legend()
 
-# Plot 3: Feature 2 (Production Distance) and Feature 3 (Time Delivery)
-fig3 = plt.figure()
-ax3 = fig3.add_subplot(111, projection="3d")
-ax3.scatter(
-    x_test_poly[:, 1], x_test_poly[:, 2], y_test[:, 0], c="b", label="Actual Price"
-)
-ax3.scatter(
-    x_test_poly[:, 1], x_test_poly[:, 2], y_pred[:, 0], c="r", label="Predicted Price"
-)
-ax3.set_xlabel("Production Distance")
-ax3.set_ylabel("Time Delivery")
-ax3.set_zlabel("Price")
-ax3.legend()
-plt.title("Price Prediction with Production Distance and Time Delivery")
-plt.savefig("results/ex10/result_ex10_figure-3.png")
-plt.close()
+plt.suptitle("Price Prediction for Different Lambda Values")
+plt.tight_layout()
+plt.savefig("results/ex07/best_degree_prediction.png")
