144309033a65e5013979f439abf6cad8ab3683b7
Lala_pukuchan <ruruover1105@gmail.com>
2023-12-29 08:43:26 +0100

    ex07 change plots

--- ex07/space_avocado.py
+++ ex07/space_avocado.py
@@ -3,7 +3,6 @@ import numpy as np
 import pickle
 import matplotlib.pyplot as plt
 
-
 # Load dataset
 data = pd.read_csv("ex07/space_avocado.csv")
 x = np.array(data[["weight", "prod_distance", "time_delivery"]])
@@ -22,32 +21,38 @@ for degree in models:
             best_mse = model_info["mse"]
             best_degree = degree
 
-# Print the best degree and MSE
-print(f"Best degree: {best_degree}, Best MSE: {best_mse}")
+# print best model
+print("best degree:", best_degree, "best_mse:", best_mse)
 
 # Get models of the best degree with different lambda values
 lambda_models = models[best_degree]
 
-# Create subplots for each lambda
-fig, axs = plt.subplots(1, len(lambda_models), figsize=(15, 5), subplot_kw={'projection': '3d'})
+# Feature pairs and labels
+feature_pairs = [(0, 1), (0, 2), (1, 2)]
+feature_labels = ["Weight", "Production Distance", "Time Delivery"]
+dot_size = 5
 
-for i, (lambda_, model_info) in enumerate(lambda_models.items()):
-    model = model_info["model"]
-    x_test_poly = model_info["x_test_poly"]
-    y_test = model_info["y_test"]
+# Iterate over each pair of features
+for f1, f2 in feature_pairs:
+    # Create a new figure for each pair
+    fig, axs = plt.subplots(1, len(lambda_models), figsize=(15, 5), subplot_kw={'projection': '3d'})
     
-    # Predict using the model
-    y_pred = model.predict_(x_test_poly)
+    for i, (lambda_, model_info) in enumerate(lambda_models.items()):
+        model = model_info["model"]
+        x_test_poly = model_info["x_test_poly"]
+        y_test = model_info["y_test"]
+        y_pred = model.predict_(x_test_poly)
 
-    # Scatter plot for actual vs predicted prices
-    axs[i].scatter(x_test_poly[:, 0], x_test_poly[:, 1], y_test[:, 0], c="b", label="Actual Price")
-    axs[i].scatter(x_test_poly[:, 0], x_test_poly[:, 1], y_pred[:, 0], c="r", label="Predicted Price")
-    axs[i].set_title(f"Lambda {lambda_}")
-    axs[i].set_xlabel("Weight")
-    axs[i].set_ylabel("Production Distance")
-    axs[i].set_zlabel("Price")
-    axs[i].legend()
+        # Plot for each lambda
+        axs[i].scatter(x_test_poly[:, f1], x_test_poly[:, f2], y_test[:, 0], c="b", label="Actual Price", s=dot_size)
+        axs[i].scatter(x_test_poly[:, f1], x_test_poly[:, f2], y_pred[:, 0], c="r", label="Predicted Price", s=dot_size)
+        axs[i].set_xlabel(feature_labels[f1])
+        axs[i].set_ylabel(feature_labels[f2])
+        axs[i].set_zlabel("Price")
+        axs[i].set_title(f"Lambda {lambda_:.1f}")
 
-plt.suptitle("Price Prediction for Different Lambda Values")
-plt.tight_layout()
-plt.savefig("results/ex07/best_degree_prediction.png")
+    # Add overall title and save the figure
+    plt.suptitle(f"Price Prediction for {feature_labels[f1]} vs {feature_labels[f2]} for Different Lambda Values on best degree {best_degree}")
+    plt.tight_layout()
+    plt.savefig(f"results/ex07/best_degree_prediction_{feature_labels[f1]}_{feature_labels[f2]}.png")
+    plt.close(fig)
